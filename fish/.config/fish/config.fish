7c35d85386e3d04c0c7fa6bce125a5b47fe27169
Federico Calore <Federico.Calore@ing.nl>
2022-01-10 16:00:36 +0100

    Update home directory reference and GCloud path settings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,18 @@
 # set fish prompt
 . ~/.config/fish/functions/fish_prompt.fish
+
 # disable updating the prompt when virtualenv activates (use custom prompt instead)
 set VIRTUAL_ENV_DISABLE_PROMPT disable
 
 # miniconda
-set PATH $PATH /Users/federico/miniconda3/bin
-source /Users/federico/miniconda3/etc/fish/conf.d/conda.fish
+#set PATH $PATH ~/miniconda3/bin
+#source ~/miniconda3/etc/fish/conf.d/conda.fish
 
 # apache maven
 set PATH $PATH /opt/apache-maven/bin
 
 # user bin files
-set PATH $PATH /Users/federico/bin
+set PATH $PATH ~/bin
 
 # git
 set -x LC_ALL en_US.UTF-8
@@ -20,9 +21,9 @@ set -x LC_ALL en_US.UTF-8
 set -x PIP_REQUIRE_VIRTUALENV true
 
 # Google Cloud SDK
-if [ -f '/Users/federico/google-cloud-sdk/path.fish.inc' ]; . '/Users/federico/google-cloud-sdk/path.fish.inc'; end
+if [ -f '~/gcloud/path.fish.inc' ]; . '~/gcloud/path.fish.inc'; end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 # Load fishmarks (http://github.com/techwizrd/fishmarks)
-. $HOME/.fishmarks/marks.fish
+#. $HOME/.fishmarks/marks.fish
